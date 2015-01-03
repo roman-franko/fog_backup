fa33c6a12a38de4bf50e8f5f140a7c16a26f46ad
Roman Franko <r.franko@drooms.com>
2015-01-03 10:50:07 +0200

    added passive for ftp pulling

--- lib/fog_backup/helpers/ftp.rb
+++ lib/fog_backup/helpers/ftp.rb
@@ -8,7 +8,10 @@ module FogBackup
         password = FogBackup.config['ftp']['password']
         ignore_list = IgnoreSource.new(FogBackup.ignore_list)
 
-        ftp = FtpSync.new(host, username, password, ignore: ignore_list)
+        ftp = FtpSync.new(host, username, password,
+          ignore: ignore_list,
+          verbose: FogBackup.config['ftp']['verbose'],
+          passive: FogBackup.config['ftp']['passive'])
         ftp.pull_dir local_dir, remote_dir
       end
     end
